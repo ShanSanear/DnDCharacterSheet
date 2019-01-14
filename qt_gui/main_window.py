3d9e4c2312a95fafc7d83b9b69f77f5f6c5886cd
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-14 22:00:00 +0100

    Added basic tabs, changed placement of widgets to be somewhat sane.

--- qt_gui/main_window.py
+++ qt_gui/main_window.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QVBoxLayout, QScrollArea
+from PyQt5.QtWidgets import QVBoxLayout, QScrollArea, QTabWidget
 
 from qt_gui.boxes.armor_items import ArmorItems
 from qt_gui.boxes.attacks import AttacksBox
@@ -21,74 +21,121 @@ from qt_gui.boxes.weapon_statistics import WeaponStatisticsBox
 from qt_gui.boxes.weapons_box import WeaponsBox
 
 
+class MyTabWidget(QtWidgets.QWidget):
+
+    def __init__(self, parent):
+        super(MyTabWidget, self).__init__(parent)
+        self.tabs = QTabWidget()
+        self.tab1 = QtWidgets.QWidget()
+        self.tab2 = QtWidgets.QWidget()
+        self.tab3 = QtWidgets.QWidget()
+        self.tabs.addTab(self.tab1, "Tab 1")
+        self.tabs.addTab(self.tab2, "Tab 2")
+        self.tabs.addTab(self.tab3, "Tab 3")
+
+        self.tab1.layout = QVBoxLayout(self)
+        self.tab1.setLayout(self.tab1.layout)
+
+        self.tab2.layout = QVBoxLayout(self)
+        self.tab2.setLayout(self.tab2.layout)
+
+        self.tab3.layout = QVBoxLayout(self)
+        self.tab3.setLayout(self.tab3.layout)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.tabs)
+
+
+
 # noinspection PyAttributeOutsideInit
 class MainWindowUi:
     def setup_ui(self, main_window):
         main_window.setObjectName("MainWindow")
-        main_window.resize(1748, 1267)
+        main_window.resize(1800, 1200)
         main_window.setSizeIncrement(QtCore.QSize(1, 0))
         main_window.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
-        self.central_widget = QtWidgets.QWidget(main_window)
+        # self.central_widget = QtWidgets.QWidget(main_window)
+        self.central_widget = MyTabWidget(main_window)
         self.central_widget.setObjectName("centralwidget")
-        layout = QtWidgets.QVBoxLayout(self.central_widget)
+        # self.layout_v_box = QtWidgets.QVBoxLayout(self.central_widget)
 
-        self.scrollArea = QtWidgets.QScrollArea(self.central_widget)
-        layout.addWidget(self.scrollArea)
+        # self.scroll_area = QtWidgets.QScrollArea(self.central_widget)
+        # self.layout_v_box.addWidget(self.scroll_area)
 
-        self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 1800, 1500))
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        # self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        # self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 1800, 1500))
+        # self.scroll_area.setWidget(self.scrollAreaWidgetContents)
+        #
+        # self.layout_h_box = QtWidgets.QHBoxLayout(self.scrollAreaWidgetContents)
 
-        layout = QtWidgets.QHBoxLayout(self.scrollAreaWidgetContents)
-        # add child widgets to this layout...
+        # self.push_button = QtWidgets.QPushButton(self.central_widget)
+        # self.push_button.setGeometry(QtCore.QRect(100, 20, 75, 23))
+        # self.push_button.setObjectName("clickMeButton")
+        # self.menu_bar = MenuBar(main_window)
+        parent_for_boxes = None
 
-        self.push_button = QtWidgets.QPushButton(self.central_widget)
-        self.push_button.setGeometry(QtCore.QRect(1540, 170, 75, 23))
-        self.push_button.setObjectName("clickMeButton")
-        self.menu_bar = MenuBar(main_window)
-        self.weapons_statistics_box = WeaponStatisticsBox(None)
-        self.skills_box = SkillsBox(None)
-        self.number_of_spells_box = NumberOfSpellsBox(None)
-        self.spells_per_day_box = SpellsPerDayBox(None)
-        self.known_spells_box = KnownSpellsBox(None)
-        self.armor_items_box = ArmorItems(None)
-        self.weapons_box = WeaponsBox(None)
-        self.notes_box = NotesBox(None)
-        self.languages_box = LanguagesBox(None)
-        self.items_box = ItemsBox(None)
-        self.initiative_speed_box = InitiativeSpeedBox(None)
-        self.feats_box = FeatsBox(None)
-        self.saving_throws_box = SavingThrowsBox(None)
-        self.attacks_box = AttacksBox(None)
-        self.hp_ac_box = HpAcBox(None)
-        self.attributes_box = AttributesBox(None)
-        self.basic_info_box = BasicInfoBox(None)
+        self.basic_info_box = BasicInfoBox(parent_for_boxes, position=[10, 10], size=[850, 200])
+        self.attributes_box = AttributesBox(parent_for_boxes, position=[10, 220], size=[320, 250])
+        self.initiative_speed_box = InitiativeSpeedBox(parent_for_boxes, position=[10, 480], size=[271, 121])
+        self.hp_ac_box = HpAcBox(parent_for_boxes, position=[350, 220], size=[450, 150])
+        self.saving_throws_box = SavingThrowsBox(parent_for_boxes, position=[350, 380], size=[450, 150])
+        self.attacks_box = AttacksBox(parent_for_boxes, position=[350, 550], size=[391, 121])
+        self.weapons_statistics_box = WeaponStatisticsBox(parent_for_boxes, position=[870, 10], size=[261, 121])
+        self.weapons_box = WeaponsBox(parent_for_boxes, position=[870, 220], size=[620, 450])
 
-        children = [self.weapons_statistics_box.root,
-        self.skills_box.root,
-        self.number_of_spells_box.root,
-        self.spells_per_day_box.root,
-        self.known_spells_box.root,
-        self.armor_items_box.root,
+        self.skills_box = SkillsBox(parent_for_boxes, position=[10, 10], size=[600, 400])
+        self.languages_box = LanguagesBox(parent_for_boxes, position=[650, 10], size=[200, 250])
+        self.feats_box = FeatsBox(parent_for_boxes, position=[650, 270], size=[200, 80])
+        self.spells_per_day_box = SpellsPerDayBox(parent_for_boxes, position=[650, 370], size=[271, 350])
+        self.number_of_spells_box = NumberOfSpellsBox(parent_for_boxes, position=[10, 420], size=[581, 131])
+        self.known_spells_box = KnownSpellsBox(parent_for_boxes, position=[10, 550], size=[581, 101])
+
+        self.notes_box = NotesBox(parent_for_boxes, position=[10, 350], size=[510, 200])
+        self.armor_items_box = ArmorItems(parent_for_boxes, position=[10, 10], size=[620, 331])
+        self.items_box = ItemsBox(parent_for_boxes, position=[650, 10], size=[581, 291])
+
+        children_1 = [
         self.weapons_box.root,
-        self.notes_box.root,
-        self.languages_box.root,
-        self.items_box.root,
-        self.initiative_speed_box.root,
-        self.feats_box.root,
         self.saving_throws_box.root,
         self.attacks_box.root,
         self.hp_ac_box.root,
         self.attributes_box.root,
-        self.basic_info_box.root]
-        for child in children:
-            layout.addChildWidget(child)
-        #main_window.setCentralWidget(self.central_widget)
-        #Xself.scrollArea.setWidgetResizable(True)
+            self.basic_info_box.root,
+            self.initiative_speed_box.root,
+            self.weapons_statistics_box.root,
+        ]
+        children_2 = [
+            self.skills_box.root,
+            self.number_of_spells_box.root,
+            self.spells_per_day_box.root,
+            self.known_spells_box.root,
+
+            self.languages_box.root,
+            self.feats_box.root,
+        ]
+
+        children_3 = [
+            self.armor_items_box.root,
+            self.notes_box.root,
+            self.items_box.root,
+        ]
+        for child in children_1:
+            self.central_widget.tab1.layout.addChildWidget(child)
+
+        for child in children_2:
+            self.central_widget.tab2.layout.addChildWidget(child)
+
+        for child in children_3:
+            self.central_widget.tab3.layout.addChildWidget(child)
+        # main_window.setCentralWidget(self.central_widget)
+        # Xself.scrollArea.setWidgetResizable(True)
+
+        self.central_widget.setLayout(self.central_widget.layout)
         self.retranslate_ui(main_window)
         main_window.setCentralWidget(self.central_widget)
+        self.central_widget.tabs.setCurrentIndex(2)
 
     def retranslate_ui(self, main_window):
         _translate = QtCore.QCoreApplication.translate
         main_window.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.push_button.setText(_translate("MainWindow", "ClickMe"))
+#        self.push_button.setText(_translate("MainWindow", "ClickMe"))
