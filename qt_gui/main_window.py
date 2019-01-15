0f22856f19b4da563e1cd89d025c02055552ee13
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-15 17:19:32 +0100

    Changed from using addChildWidget to using parent and divided widgets using functions representing widgets for specific tab.

--- qt_gui/main_window.py
+++ qt_gui/main_window.py
@@ -52,11 +52,21 @@ class MainWindowUi:
     def setup_ui(self, main_window):
         main_window.setObjectName("MainWindow")
         main_window.resize(1800, 1200)
-        main_window.setSizeIncrement(QtCore.QSize(1, 0))
-        main_window.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
-        # self.central_widget = QtWidgets.QWidget(main_window)
         self.central_widget = MyTabWidget(main_window)
         self.central_widget.setObjectName("centralwidget")
+
+        self.add_widgets_to_tab_1()
+        self.add_widgets_to_tab_2()
+        self.add_widgets_to_tab_3()
+
+        self.central_widget.setLayout(self.central_widget.layout)
+        self.retranslate_ui(main_window)
+        main_window.setCentralWidget(self.central_widget)
+        self.central_widget.tabs.setCurrentIndex(0)
+
+        # main_window.setSizeIncrement(QtCore.QSize(1, 0))
+        # main_window.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
+        # self.central_widget = QtWidgets.QWidget(main_window)
         # self.layout_v_box = QtWidgets.QVBoxLayout(self.central_widget)
 
         # self.scroll_area = QtWidgets.QScrollArea(self.central_widget)
@@ -72,68 +82,36 @@ class MainWindowUi:
         # self.push_button.setGeometry(QtCore.QRect(100, 20, 75, 23))
         # self.push_button.setObjectName("clickMeButton")
         # self.menu_bar = MenuBar(main_window)
-        parent_for_boxes = None
+        # main_window.setCentralWidget(self.central_widget)
+        # Xself.scrollArea.setWidgetResizable(True)
 
+    def add_widgets_to_tab_1(self):
+        parent_for_boxes = self.central_widget.tab1
         self.basic_info_box = BasicInfoBox(parent_for_boxes, position=[10, 10], size=[850, 200])
         self.attributes_box = AttributesBox(parent_for_boxes, position=[10, 220], size=[320, 250])
         self.initiative_speed_box = InitiativeSpeedBox(parent_for_boxes, position=[10, 480], size=[271, 121])
-        self.hp_ac_box = HpAcBox(parent_for_boxes, position=[350, 220], size=[450, 150])
+        self.hp_ac_box = HpAcBox(parent_for_boxes, position=[350, 220], size=[350, 150])
         self.saving_throws_box = SavingThrowsBox(parent_for_boxes, position=[350, 380], size=[450, 150])
         self.attacks_box = AttacksBox(parent_for_boxes, position=[350, 550], size=[391, 121])
         self.weapons_statistics_box = WeaponStatisticsBox(parent_for_boxes, position=[870, 10], size=[261, 121])
         self.weapons_box = WeaponsBox(parent_for_boxes, position=[870, 220], size=[620, 450])
+        return parent_for_boxes
 
+    def add_widgets_to_tab_2(self):
+        parent_for_boxes = self.central_widget.tab2
         self.skills_box = SkillsBox(parent_for_boxes, position=[10, 10], size=[600, 400])
-        self.languages_box = LanguagesBox(parent_for_boxes, position=[650, 10], size=[200, 250])
-        self.feats_box = FeatsBox(parent_for_boxes, position=[650, 270], size=[200, 80])
-        self.spells_per_day_box = SpellsPerDayBox(parent_for_boxes, position=[650, 370], size=[271, 350])
-        self.number_of_spells_box = NumberOfSpellsBox(parent_for_boxes, position=[10, 420], size=[581, 131])
-        self.known_spells_box = KnownSpellsBox(parent_for_boxes, position=[10, 550], size=[581, 101])
+        self.number_of_spells_box = NumberOfSpellsBox(parent_for_boxes, position=[10, 420], size=[600, 130])
+        self.languages_box = LanguagesBox(parent_for_boxes, position=[650, 10], size=[270, 250])
+        self.feats_box = FeatsBox(parent_for_boxes, position=[10, 580], size=[270, 80])
+        self.spells_per_day_box = SpellsPerDayBox(parent_for_boxes, position=[950, 10], size=[300, 350])
+        self.known_spells_box = KnownSpellsBox(parent_for_boxes, position=[650, 380], size=[600, 100])
+        return parent_for_boxes
 
+    def add_widgets_to_tab_3(self):
+        parent_for_boxes = self.central_widget.tab3
         self.notes_box = NotesBox(parent_for_boxes, position=[10, 350], size=[510, 200])
         self.armor_items_box = ArmorItems(parent_for_boxes, position=[10, 10], size=[620, 331])
         self.items_box = ItemsBox(parent_for_boxes, position=[650, 10], size=[581, 291])
-
-        children_1 = [
-        self.weapons_box.root,
-        self.saving_throws_box.root,
-        self.attacks_box.root,
-        self.hp_ac_box.root,
-        self.attributes_box.root,
-            self.basic_info_box.root,
-            self.initiative_speed_box.root,
-            self.weapons_statistics_box.root,
-        ]
-        children_2 = [
-            self.skills_box.root,
-            self.number_of_spells_box.root,
-            self.spells_per_day_box.root,
-            self.known_spells_box.root,
-
-            self.languages_box.root,
-            self.feats_box.root,
-        ]
-
-        children_3 = [
-            self.armor_items_box.root,
-            self.notes_box.root,
-            self.items_box.root,
-        ]
-        for child in children_1:
-            self.central_widget.tab1.layout.addChildWidget(child)
-
-        for child in children_2:
-            self.central_widget.tab2.layout.addChildWidget(child)
-
-        for child in children_3:
-            self.central_widget.tab3.layout.addChildWidget(child)
-        # main_window.setCentralWidget(self.central_widget)
-        # Xself.scrollArea.setWidgetResizable(True)
-
-        self.central_widget.setLayout(self.central_widget.layout)
-        self.retranslate_ui(main_window)
-        main_window.setCentralWidget(self.central_widget)
-        self.central_widget.tabs.setCurrentIndex(2)
 
     def retranslate_ui(self, main_window):
         _translate = QtCore.QCoreApplication.translate
