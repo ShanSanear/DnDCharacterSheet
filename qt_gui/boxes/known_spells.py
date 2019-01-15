dce8efb8131a9f711ea1aa16449d876e19c82652
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-15 22:02:35 +0100

    Similar to skills - added function for adding new elements and updating size.

--- qt_gui/boxes/known_spells.py
+++ qt_gui/boxes/known_spells.py
@@ -1,3 +1,5 @@
+from types import SimpleNamespace
+
 from PyQt5 import QtWidgets, QtCore
 
 from qt_gui.boxes.qt_generic_functions import create_qlabel, create_qline_edit
@@ -12,61 +14,84 @@ class KnownSpellsBox:
         self.root.setGeometry(QtCore.QRect(*position, *size))
         self.root.setObjectName("KnownSpellsBox")
         self.container = QtWidgets.QWidget(self.root)
-        self.container.setGeometry(QtCore.QRect(10, 20, 561, 61))
         self.container.setObjectName("gridLayoutWidget_11")
         self.layout = QtWidgets.QGridLayout(self.container)
-        self.layout.setContentsMargins(9, 9, 9, 9)
-        self.layout.setSpacing(6)
         self.layout.setObjectName("KnownSpellsLayout")
-        qline_dict = dict(parent=self.container, min_size=(0, 23), max_size=(20, 16777215))
-        qline_dict_2 = dict(parent=self.container, min_size=(0, 23))
+
+        self.spells = []
         qlabel_dict = dict(parent=self.container, max_size=(20, 16777215))
         qlabel_dict_2 = dict(parent=self.container)
 
-        self.known_spells_lvl_label_1 = create_qlabel("known_spells_lvl_label_1", **qlabel_dict)
+        self.known_spells_lvl_label = create_qlabel("known_spells_lvl_label_1", **qlabel_dict)
 
-        self.known_spells_name_label_1 = create_qlabel("known_spells_name_label_1", **qlabel_dict_2)
+        self.known_spells_name_label = create_qlabel("known_spells_name_label_1", **qlabel_dict_2)
 
-        self.known_spells_description_button_label_1 = create_qlabel("known_spells_description_label_1",
-                                                                     **qlabel_dict_2)
-        self.known_spells_description_field_label_1 = create_qlabel("known_spells_description_field_label_1",
-                                                                    **qlabel_dict_2)
+        self.known_spells_description_button_label = create_qlabel("known_spells_description_label_1",
+                                                                   **qlabel_dict_2)
+        self.known_spells_description_field_label = create_qlabel("known_spells_description_field_label_1",
+                                                                  **qlabel_dict_2)
 
-        self.known_spells_1_lvl = create_qline_edit("known_spells_1_lvl", **qline_dict)
-        self.known_spells_1_name = create_qline_edit("known_spells_1_name", **qline_dict_2)
-
-        self.known_spells_1_description_field = QtWidgets.QLineEdit(self.container)
-        self.known_spells_1_description_field.setObjectName("known_spells_1_description_field")
-
-        self.known_spells_1_description_field.setMinimumSize(QtCore.QSize(350, 23))
-
-        self.known_spells_1_description_button = QtWidgets.QPushButton(self.container)
-        self.known_spells_1_description_button.setObjectName("known_spells_1_description_button")
-        self.known_spells_1_description_button.setMaximumWidth(20)
+        for _ in range(15):
+            self.add_spell()
         self.add_to_layout()
         self.translate()
         self.root.setLayout(self.layout)
 
-    def add_to_layout(self):
-        self.layout.addWidget(self.known_spells_lvl_label_1, 0, 0, 1, 1)
-        self.layout.addWidget(self.known_spells_name_label_1, 0, 1, 1, 1)
-        self.layout.addWidget(self.known_spells_description_field_label_1, 0, 2, 1, 1)
-        self.layout.addWidget(self.known_spells_description_button_label_1, 0, 3, 1, 1)
+    def create_spell(self):
+        qline_dict = dict(parent=self.container, max_size=(20, 16777215))
+        qline_dict_2 = dict(parent=self.container, min_size=(0, 10))
+        new_spell = SimpleNamespace()
+        new_spell.lvl = create_qline_edit("known_spells_1_lvl", **qline_dict)
+        new_spell.lvl.setText("10")
+        new_spell.name = create_qline_edit("known_spells_1_name", **qline_dict_2)
+        new_spell.name.setText("Lorem ipsum")
+        new_spell.description_field = QtWidgets.QLineEdit(self.container)
+        new_spell.description_field.setObjectName("known_spells_1_description_field")
+        new_spell.description_field.setMinimumSize(QtCore.QSize(350, 23))
+        new_spell.description_field.setText(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien urna, egestas eu tempor at, pretium nec orci. In nec pharetra tellus. In malesuada erat tellus, eget efficitur elit convallis eu.")
+        new_spell.description_button = QtWidgets.QPushButton(self.container)
+        new_spell.description_button.setObjectName("known_spells_1_description_button")
+        new_spell.description_button.setMaximumWidth(20)
+        new_spell.description_button.setText("...")
+        return new_spell
 
-        self.layout.addWidget(self.known_spells_1_lvl, 1, 0, 1, 1)
-        self.layout.addWidget(self.known_spells_1_name, 1, 1, 1, 1)
-        self.layout.addWidget(self.known_spells_1_description_field, 1, 2, 1, 1)
-        self.layout.addWidget(self.known_spells_1_description_button, 1, 3, 1, 1)
+    def add_spell(self):
+        spell_idx = len(self.spells)
+        new_spell = self.create_spell()
+        self.spells.append(new_spell)
+        for el_idx, element in enumerate(new_spell.__dict__.values()):
+            self.layout.addWidget(element, spell_idx + 1, el_idx, 1, 1)
+        self.update_container_size()
+
+    def add_to_layout(self):
+        self.layout.addWidget(self.known_spells_lvl_label, 0, 0, 1, 1)
+        self.layout.addWidget(self.known_spells_name_label, 0, 1, 1, 1)
+        self.layout.addWidget(self.known_spells_description_field_label, 0, 2, 1, 1)
+        self.layout.addWidget(self.known_spells_description_button_label, 0, 3, 1, 1)
+
+        # self.layout.addWidget(self.known_spells_1_lvl, 1, 0, 1, 1)
+        # self.layout.addWidget(self.known_spells_1_name, 1, 1, 1, 1)
+        # self.layout.addWidget(self.known_spells_1_description_field, 1, 2, 1, 1)
+        # self.layout.addWidget(self.known_spells_1_description_button, 1, 3, 1, 1)
 
     def translate(self):
         _translate = QtCore.QCoreApplication.translate
         self.root.setTitle(_translate("MainWindow", "Known Spells"))
-        self.known_spells_1_lvl.setText(_translate("MainWindow", "10"))
-        self.known_spells_1_name.setText(_translate("MainWindow", "Oczyszczenie jedzenia i wody"))
-        self.known_spells_lvl_label_1.setText(_translate("MainWindow", "LVL"))
-        self.known_spells_name_label_1.setText(_translate("MainWindow", "Spell name"))
-        self.known_spells_description_button_label_1.setText(_translate("MainWindow", "Desc"))
-        self.known_spells_1_description_button.setText(_translate("MainWindow", "..."))
-        self.known_spells_description_field_label_1.setText("DESC")
-        self.known_spells_1_description_field.setText(
-            " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien urna, egestas eu tempor at, pretium nec orci. In nec pharetra tellus. In malesuada erat tellus, eget efficitur elit convallis eu. Integer consectetur porttitor eros vitae sagittis. Vestibulum commodo suscipit varius. Nulla vitae fringilla velit. Mauris sagittis tellus urna. Class aptent tac")
+        self.known_spells_lvl_label.setText(_translate("MainWindow", "LVL"))
+        self.known_spells_name_label.setText(_translate("MainWindow", "Spell name"))
+        self.known_spells_description_button_label.setText(_translate("MainWindow", "Desc"))
+        self.known_spells_description_field_label.setText("DESC")
+        # self.known_spells_1_lvl.setText(_translate("MainWindow", "10"))
+        # self.known_spells_1_name.setText(_translate("MainWindow", "Oczyszczenie jedzenia i wody"))
+        # self.known_spells_1_description_button.setText(_translate("MainWindow", "..."))
+        # self.known_spells_1_description_field.setText(
+        #     " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien urna, egestas eu tempor at, pretium nec orci. In nec pharetra tellus. In malesuada erat tellus, eget efficitur elit convallis eu. Integer consectetur porttitor eros vitae sagittis. Vestibulum commodo suscipit varius. Nulla vitae fringilla velit. Mauris sagittis tellus urna. Class aptent tac")
+
+    def update_container_size(self):
+        root_width = self.root.width()
+        root_height = self.root.height()
+        container_width = self.container.width()
+        container_height = self.container.height()
+        self.root.setMinimumSize(QtCore.QSize(root_width, root_height + 30))
+        self.container.setMinimumSize(QtCore.QSize(container_width, container_height + 30))
