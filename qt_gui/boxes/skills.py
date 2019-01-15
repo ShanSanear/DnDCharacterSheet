fa0d9b2392ad39d73c08857880dde81cc1e135de
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-15 21:46:10 +0100

    Generalized adding new skills, added robust resizing of both root element and container to take into account new skill lines.

--- qt_gui/boxes/skills.py
+++ qt_gui/boxes/skills.py
@@ -1,3 +1,5 @@
+from types import SimpleNamespace
+
 from PyQt5 import QtWidgets, QtCore
 
 from qt_gui.boxes.box import DefaultBox
@@ -20,39 +22,85 @@ class SkillsBox(DefaultBox):
         self.layout.setSpacing(6)
         self.root.setMaximumHeight(100)
         self.layout.setObjectName("SkillsLayout")
+        self.skills = []
+
+        self.create_labels()
+        for _ in range(10):
+            self.add_skill()
+
+        self.add_to_layout()
+        self.translate()
+        self.set_default()
+        self.root.setLayout(self.layout)
+
+    def create_new_skill(self):
+        new_skill = SimpleNamespace()
+        skill_idx = len(self.skills)
+        new_skill.skill_name = QtWidgets.QLineEdit(self.container)
+        new_skill.skill_name.setMinimumSize(QtCore.QSize(0, 23))
+        new_skill.skill_name.setObjectName(f"skills{skill_idx}skill_name")
+        new_skill.skill_name.setText(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien urna, egestas eu tempor at, pretium nec orci.")
+
+        new_skill.attr_choice = QtWidgets.QComboBox(self.container)
+        new_skill.attr_choice.setMaximumSize(QtCore.QSize(69, 20))
+        new_skill.attr_choice.setObjectName(f"skills{skill_idx}attr_choice")
+        new_skill.attr_choice.addItem("STR")
+        new_skill.attr_choice.addItem("DEX")
+        new_skill.attr_choice.addItem("CON")
+        new_skill.attr_choice.addItem("INT")
+        new_skill.attr_choice.addItem("WIS")
+        new_skill.attr_choice.addItem("CHA")
+
+        new_skill.total = QtWidgets.QLineEdit(self.container)
+        new_skill.total.setMinimumSize(QtCore.QSize(0, 23))
+        new_skill.total.setMaximumSize(QtCore.QSize(54, 20))
+        new_skill.total.setAlignment(QtCore.Qt.AlignCenter)
+        new_skill.total.setObjectName(f"skills{skill_idx}total")
+        new_skill.total.setText("10")
+
+        new_skill.attr_mod = QtWidgets.QLineEdit(self.container)
+        new_skill.attr_mod.setEnabled(False)
+        new_skill.attr_mod.setMinimumSize(QtCore.QSize(0, 23))
+        new_skill.attr_mod.setMaximumSize(QtCore.QSize(54, 20))
+        new_skill.attr_mod.setAutoFillBackground(False)
+        new_skill.attr_mod.setAlignment(QtCore.Qt.AlignCenter)
+        new_skill.attr_mod.setReadOnly(True)
+        new_skill.attr_mod.setObjectName(f"skills{skill_idx}attr_mod")
+        new_skill.attr_mod.setText("10")
+
+        new_skill.rank = QtWidgets.QLineEdit(self.container)
+        new_skill.rank.setMinimumSize(QtCore.QSize(0, 23))
+        new_skill.rank.setMaximumSize(QtCore.QSize(54, 20))
+        new_skill.rank.setAlignment(QtCore.Qt.AlignCenter)
+        new_skill.rank.setObjectName(f"skills{skill_idx}rank")
+        new_skill.rank.setText("10")
+
+        new_skill.misc_mod = QtWidgets.QLineEdit(self.container)
+        new_skill.misc_mod.setMinimumSize(QtCore.QSize(0, 23))
+        new_skill.misc_mod.setMaximumSize(QtCore.QSize(54, 20))
+        new_skill.misc_mod.setAlignment(QtCore.Qt.AlignCenter)
+        new_skill.misc_mod.setObjectName(f"skills_{skill_idx}_misc_mod")
+        new_skill.misc_mod.setText("10")
+
+        new_skill.cross_class_checkbox = QtWidgets.QCheckBox(self.container)
+        new_skill.cross_class_checkbox.setObjectName(f"skills{skill_idx}description_button")
+
+        return new_skill
+
+    def add_skill(self):
+        skill_idx = len(self.skills)
+        new_skill = self.create_new_skill()
+        self.skills.append(new_skill)
+        for el_idx, element in enumerate(new_skill.__dict__.values()):
+            self.layout.addWidget(element, skill_idx + 1, el_idx, 1, 1)
+        self.update_container_size()
+
+    def create_labels(self):
         self.skills_cross_class_label = QtWidgets.QLabel(self.container)
         self.skills_cross_class_label.setMaximumSize(QtCore.QSize(16777215, 20))
         self.skills_cross_class_label.setAlignment(QtCore.Qt.AlignCenter)
         self.skills_cross_class_label.setObjectName("skills_description_label")
-        self.skills_1_cross_class_checkbox = QtWidgets.QCheckBox(self.container)
-        self.skills_1_cross_class_checkbox.setObjectName("skills_1_description_button")
-        self.skills_1_cross_class_checkbox.setMaximumWidth(20)
-        self.skills_1_misc_mod = QtWidgets.QLineEdit(self.container)
-        self.skills_1_misc_mod.setMinimumSize(QtCore.QSize(0, 23))
-        self.skills_1_misc_mod.setMaximumSize(QtCore.QSize(54, 20))
-        self.skills_1_misc_mod.setAlignment(QtCore.Qt.AlignCenter)
-        self.skills_1_misc_mod.setObjectName("skills_1_misc_mod")
-        self.skills_1_attr_mod = QtWidgets.QLineEdit(self.container)
-        self.skills_1_attr_mod.setEnabled(False)
-        self.skills_1_attr_mod.setMinimumSize(QtCore.QSize(0, 23))
-        self.skills_1_attr_mod.setMaximumSize(QtCore.QSize(54, 20))
-        self.skills_1_attr_mod.setAutoFillBackground(False)
-        self.skills_1_attr_mod.setAlignment(QtCore.Qt.AlignCenter)
-        self.skills_1_attr_mod.setReadOnly(True)
-        self.skills_1_attr_mod.setObjectName("skills_1_attr_mod")
-        self.skills_1_rank = QtWidgets.QLineEdit(self.container)
-        self.skills_1_rank.setMinimumSize(QtCore.QSize(0, 23))
-        self.skills_1_rank.setMaximumSize(QtCore.QSize(54, 20))
-        self.skills_1_rank.setAlignment(QtCore.Qt.AlignCenter)
-        self.skills_1_rank.setObjectName("skills_1_rank")
-        self.skills_1_total = QtWidgets.QLineEdit(self.container)
-        self.skills_1_total.setMinimumSize(QtCore.QSize(0, 23))
-        self.skills_1_total.setMaximumSize(QtCore.QSize(54, 20))
-        self.skills_1_total.setAlignment(QtCore.Qt.AlignCenter)
-        self.skills_1_total.setObjectName("skills_1_total")
-        self.skills_1_skill_name = QtWidgets.QLineEdit(self.container)
-        self.skills_1_skill_name.setMinimumSize(QtCore.QSize(0, 23))
-        self.skills_1_skill_name.setObjectName("skills_1_skill_name")
         self.skills_rank_label = QtWidgets.QLabel(self.container)
         self.skills_rank_label.setMaximumSize(QtCore.QSize(54, 20))
         self.skills_rank_label.setAlignment(QtCore.Qt.AlignCenter)
@@ -65,15 +113,6 @@ class SkillsBox(DefaultBox):
         self.skills_misc_label.setMaximumSize(QtCore.QSize(54, 20))
         self.skills_misc_label.setAlignment(QtCore.Qt.AlignCenter)
         self.skills_misc_label.setObjectName("skills_misc_label")
-        self.skills_1_attr_choice = QtWidgets.QComboBox(self.container)
-        self.skills_1_attr_choice.setMaximumSize(QtCore.QSize(69, 20))
-        self.skills_1_attr_choice.setObjectName("skills_1_attr_choice")
-        self.skills_1_attr_choice.addItem("")
-        self.skills_1_attr_choice.addItem("")
-        self.skills_1_attr_choice.addItem("")
-        self.skills_1_attr_choice.addItem("")
-        self.skills_1_attr_choice.addItem("")
-        self.skills_1_attr_choice.addItem("")
         self.skills_skill_name_label = QtWidgets.QLabel(self.container)
         self.skills_skill_name_label.setMaximumSize(QtCore.QSize(16777215, 20))
         self.skills_skill_name_label.setObjectName("skills_skill_name_label")
@@ -86,23 +125,11 @@ class SkillsBox(DefaultBox):
         self.skills_total_label.setAlignment(QtCore.Qt.AlignCenter)
         self.skills_total_label.setObjectName("skills_total_label")
 
-        self.add_to_layout()
-        self.translate()
-        self.set_default()
-        self.root.setLayout(self.layout)
-
     def add_to_layout(self):
         self.layout.addWidget(self.skills_cross_class_label, 0, 6, 1, 1)
-        self.layout.addWidget(self.skills_1_cross_class_checkbox, 1, 6, 1, 1)
-        self.layout.addWidget(self.skills_1_misc_mod, 1, 5, 1, 1)
-        self.layout.addWidget(self.skills_1_attr_mod, 1, 3, 1, 1)
-        self.layout.addWidget(self.skills_1_rank, 1, 4, 1, 1)
-        self.layout.addWidget(self.skills_1_total, 1, 2, 1, 1)
-        self.layout.addWidget(self.skills_1_skill_name, 1, 0, 1, 1)
         self.layout.addWidget(self.skills_rank_label, 0, 4, 1, 1)
         self.layout.addWidget(self.skills_attr_mod_label, 0, 3, 1, 1)
         self.layout.addWidget(self.skills_misc_label, 0, 5, 1, 1)
-        self.layout.addWidget(self.skills_1_attr_choice, 1, 1, 1, 1)
         self.layout.addWidget(self.skills_skill_name_label, 0, 0, 1, 1)
         self.layout.addWidget(self.skills_attr_choice_label, 0, 1, 1, 1)
         self.layout.addWidget(self.skills_total_label, 0, 2, 1, 1)
@@ -111,23 +138,20 @@ class SkillsBox(DefaultBox):
         _translate = QtCore.QCoreApplication.translate
         self.root.setTitle(_translate("MainWindow", "Skills"))
         self.skills_cross_class_label.setText(_translate("MainWindow", "CC"))
-        self.skills_1_cross_class_checkbox.setText(_translate("MainWindow", "..."))
         self.skills_rank_label.setText(_translate("MainWindow", "Rank"))
         self.skills_attr_mod_label.setText(_translate("MainWindow", "Attr mod"))
         self.skills_misc_label.setText(_translate("MainWindow", "Misc"))
         self.skills_skill_name_label.setText(_translate("MainWindow", "Skill name"))
         self.skills_attr_choice_label.setText(_translate("MainWindow", "Attr"))
         self.skills_total_label.setText(_translate("MainWindow", "Total"))
-        self.skills_1_attr_choice.setItemText(0, _translate("MainWindow", "STR"))
-        self.skills_1_attr_choice.setItemText(1, _translate("MainWindow", "DEX"))
-        self.skills_1_attr_choice.setItemText(2, _translate("MainWindow", "CON"))
-        self.skills_1_attr_choice.setItemText(3, _translate("MainWindow", "INT"))
-        self.skills_1_attr_choice.setItemText(4, _translate("MainWindow", "WIS"))
-        self.skills_1_attr_choice.setItemText(5, _translate("MainWindow", "CHA"))
 
     def set_default(self):
-        self.skills_1_misc_mod.setText("10")
-        self.skills_1_attr_mod.setText("10")
-        self.skills_1_rank.setText("10")
-        self.skills_1_total.setText("10")
-        self.skills_1_skill_name.setText("Spostrzegawczosc")
+        pass
+
+    def update_container_size(self):
+        root_width = self.root.width()
+        root_height = self.root.height()
+        container_width = self.container.width()
+        container_height = self.container.height()
+        self.root.setMinimumSize(QtCore.QSize(root_width, root_height + 40))
+        self.container.setMinimumSize(QtCore.QSize(container_width, container_height + 40))
