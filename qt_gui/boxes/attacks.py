c98ec1b2d7cb5d930af20676d625ab27393e6991
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-15 18:10:27 +0100

    Generalized translation, two rows of widget added to function. Root group box is getting set layout to let remove unneeded lines of code (setting geometry, margins, spacing).

--- qt_gui/boxes/attacks.py
+++ qt_gui/boxes/attacks.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtWidgets, QtCore
 
 from qt_gui.boxes.box import DefaultBox
-from qt_gui.boxes.qt_generic_functions import create_qline_edit, create_qlabel
+from qt_gui.boxes.qt_generic_functions import create_qline_edit, create_qlabel, add_multiple_elements_to_layout_by_row, \
+    set_text_of_children
 
 
 class AttacksBox(DefaultBox):
@@ -10,12 +11,21 @@ class AttacksBox(DefaultBox):
         self.root.setGeometry(QtCore.QRect(*position, *size))
         self.root.setObjectName("AttacksBox")
         self.container = QtWidgets.QWidget(self.root)
-        self.container.setGeometry(QtCore.QRect(20, 20, 361, 91))
         self.container.setObjectName("layoutWidget1")
         self.layout = QtWidgets.QGridLayout(self.container)
-        self.layout.setContentsMargins(9, 9, 9, 9)
-        self.layout.setSpacing(6)
         self.layout.setObjectName("AttacksLayout")
+        self.translate_reference = {
+            "EN":
+                {
+                    "total_label": "Total",
+                    "base_label": "Base",
+                    "attr_mod_label": "Attr mod",
+                    "size_label": "Size",
+                    "misc_label": "Misc",
+                    "melee_label": "Melee",
+                    "ranged_label": "Ranged",
+                }
+        }
         qline_dict = dict(parent=self.container, min_size=(39, 23), max_size=(167, 20))
         qlabel_dict = dict(parent=self.container, min_size=(39, 17), max_size=(16777215, 20))
         qlabel_sign_dict = dict(parent=self.container, min_size=(8, 10), max_size=(10, 20))
@@ -48,51 +58,50 @@ class AttacksBox(DefaultBox):
         self._plus_sign_20 = create_qlabel("_plus_sign_20", **qlabel_sign_dict)
         self._plus_sign_21 = create_qlabel("_plus_sign_21", **qlabel_sign_dict)
 
-
         self.add_to_layout()
-        self.translate()
+        self.translate("EN")
         self.set_signs_labels()
         self.set_default_values()
+        self.root.setLayout(self.layout)
 
     def add_to_layout(self):
+        # TODO not sure how to change those to fit some kind of pattern, if at all
         self.layout.addWidget(self.total_label, 0, 1, 1, 1)
         self.layout.addWidget(self.base_label, 0, 3, 1, 1)
         self.layout.addWidget(self.attr_mod_label, 0, 5, 1, 1)
         self.layout.addWidget(self.size_label, 0, 7, 1, 1)
         self.layout.addWidget(self.misc_label, 0, 9, 1, 1)
-        self.layout.addWidget(self.melee_label, 1, 0, 1, 1)
-        self.layout.addWidget(self.melee_total, 1, 1, 1, 1)
-        self.layout.addWidget(self._eq_sign, 1, 2, 1, 1)
-        self.layout.addWidget(self.melee_base, 1, 3, 1, 1)
-        self.layout.addWidget(self._plus_sign_16, 1, 4, 1, 1)
-        self.layout.addWidget(self.melee_attr_mod, 1, 5, 1, 1)
-        self.layout.addWidget(self._plus_sign_17, 1, 6, 1, 1)
-        self.layout.addWidget(self.melee_size, 1, 7, 1, 1)
-        self.layout.addWidget(self._plus_sign_18, 1, 8, 1, 1)
-        self.layout.addWidget(self.melee_misc, 1, 9, 1, 1)
-        self.layout.addWidget(self.ranged_label, 2, 0, 1, 1)
-        self.layout.addWidget(self.ranged_total, 2, 1, 1, 1)
-        self.layout.addWidget(self._eq_sign_2, 2, 2, 1, 1)
-        self.layout.addWidget(self.ranged_base, 2, 3, 1, 1)
-        self.layout.addWidget(self._plus_sign_19, 2, 4, 1, 1)
-        self.layout.addWidget(self.ranged_attr_mod, 2, 5, 1, 1)
-        self.layout.addWidget(self._plus_sign_20, 2, 6, 1, 1)
-        self.layout.addWidget(self.ranged_size, 2, 7, 1, 1)
-        self.layout.addWidget(self._plus_sign_21, 2, 8, 1, 1)
-        self.layout.addWidget(self.ranged_misc, 2, 9, 1, 1)
 
-    def translate(self):
-        _translate = QtCore.QCoreApplication.translate
-        self.root.setTitle(_translate("MainWindow", "Attacks"))
-        self.total_label.setText(_translate("MainWindow", "Total"))
-        self.base_label.setText(_translate("MainWindow", "Base"))
-        self.attr_mod_label.setText(_translate("MainWindow", "Attr mod"))
-        self.size_label.setText(_translate("MainWindow", "Size"))
-        self.misc_label.setText(_translate("MainWindow", "Misc"))
-        self.melee_label.setText(_translate("MainWindow", "Melee"))
-        self.ranged_label.setText(_translate("MainWindow", "Ranged"))
+        first_row = [self.melee_label,
+                     self.melee_total,
+                     self._eq_sign,
+                     self.melee_base,
+                     self._plus_sign_16,
+                     self.melee_attr_mod,
+                     self._plus_sign_17,
+                     self.melee_size,
+                     self._plus_sign_18,
+                     self.melee_misc, ]
+
+        second_row = [self.ranged_label,
+                      self.ranged_total,
+                      self._eq_sign_2,
+                      self.ranged_base,
+                      self._plus_sign_19,
+                      self.ranged_attr_mod,
+                      self._plus_sign_20,
+                      self.ranged_size,
+                      self._plus_sign_21,
+                      self.ranged_misc, ]
+
+        add_multiple_elements_to_layout_by_row(self.layout, first_row, row=1)
+        add_multiple_elements_to_layout_by_row(self.layout, second_row, row=2)
+
+    def translate(self, language_ref):
+        set_text_of_children(self, self.translate_reference[language_ref])
 
     def set_signs_labels(self):
+        # TODO not sure how to bite this one
         self._eq_sign.setText("=")
         self._plus_sign_16.setText("+")
         self._plus_sign_17.setText("+")
@@ -103,13 +112,14 @@ class AttacksBox(DefaultBox):
         self._plus_sign_21.setText("+")
 
     def set_default_values(self):
-        self.melee_total.setText("10")
-        self.melee_base.setText("10")
-        self.melee_attr_mod.setText("10")
-        self.melee_size.setText("10")
-        self.melee_misc.setText("10")
-        self.ranged_total.setText("10")
-        self.ranged_base.setText("10")
-        self.ranged_attr_mod.setText("10")
-        self.ranged_size.setText("10")
-        self.ranged_misc.setText("10")
+        defaults = {"melee_total": "10",
+                    "melee_base": "10",
+                    "melee_attr_mod": "10",
+                    "melee_size": "10",
+                    "melee_misc": "10",
+                    "ranged_total": "10",
+                    "ranged_base": "10",
+                    "ranged_attr_mod": "10",
+                    "ranged_size": "10",
+                    "ranged_misc": "10", }
+        set_text_of_children(self, defaults)
