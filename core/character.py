860bf52a99b16e8ab9b83a628ab686be7ab146ce
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-23 21:09:09 +0100

    Added basic elements that would need testing.

--- core/character.py
+++ core/character.py
@@ -4,6 +4,11 @@ from core.attributes import Attributes
 from core.character_basic_info import CharacterBasicInfo
 from core.items import Equipment
 from core.saving_throws import SavingThrows
+from core.spells.known_spells import KnownSpells
+from core.spells.remembered_spells import RememberedSpells
+from core.spells.spell_book import SpellBook
+from core.spells.spells_dc import SpellsDc
+from core.spells.spells_per_day import SpellsPerDay
 
 
 class Character(CharacterBasicInfo):
@@ -14,4 +19,11 @@ class Character(CharacterBasicInfo):
         self.attributes = Attributes()
         self.saving_throws = SavingThrows(self.attributes)
         self.melee_attack = MeleeAttack(self.attributes)
+        self.ranged_attack = RangedAttack(self.attributes)
+        self.equipement = Equipment()
+        self.spell_book = SpellBook()
+        self.spells_dc = SpellsDc()
+        self.spells_per_day = SpellsPerDay()
+        self.remembered_spells = RememberedSpells()
+        self.known_spells = KnownSpells()
 
