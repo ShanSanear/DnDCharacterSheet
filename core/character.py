dfe500fb23dff253f24260518893fb377b759239
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-23 21:32:12 +0100

    Added missing attributes passing to other classes.

--- core/character.py
+++ core/character.py
@@ -4,6 +4,7 @@ from core.attributes import Attributes
 from core.character_basic_info import CharacterBasicInfo
 from core.items import Equipment
 from core.saving_throws import SavingThrows
+from core.skills import Skill
 from core.spells.known_spells import KnownSpells
 from core.spells.remembered_spells import RememberedSpells
 from core.spells.spell_book import SpellBook
@@ -22,8 +23,11 @@ class Character(CharacterBasicInfo):
         self.ranged_attack = RangedAttack(self.attributes)
         self.equipement = Equipment()
         self.spell_book = SpellBook()
-        self.spells_dc = SpellsDc()
-        self.spells_per_day = SpellsPerDay()
-        self.remembered_spells = RememberedSpells()
+        self.spells_dc = SpellsDc(attr_cls=self.attributes)
+        self.spells_per_day = SpellsPerDay(attr_cls=self.attributes)
+        self.remembered_spells = RememberedSpells(spell_book=self.spell_book)
         self.known_spells = KnownSpells()
+        self.skills = []
 
+    def add_new_skill(self, **kwargs):
+        self.skills.append(Skill(attr_cls=self.attributes, **kwargs))
