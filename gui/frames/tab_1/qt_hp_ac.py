aa6fe28d8350290c1cc9cc3493d46a01ee39be49
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-01-30 17:41:11 +0100

    Generalized how update of total for ac works.

--- gui/frames/tab_1/qt_hp_ac.py
+++ gui/frames/tab_1/qt_hp_ac.py
@@ -3,7 +3,7 @@ from PyQt5 import QtWidgets, QtCore
 from core.character import Character
 from gui.frames.qt_generic_classes import DefaultBox
 from gui.frames.qt_generic_functions import create_qlabel, create_qline_edit, add_element_to_layout, \
-    add_multiple_elements_to_layout_by_row, set_text_of_children
+    add_multiple_elements_to_layout_by_row, set_text_of_children, update_texts
 
 
 class HpAcBox(DefaultBox):
@@ -46,12 +46,13 @@ class HpAcBox(DefaultBox):
                     "ac_size_bonus_label": "Size",
                     "ac_misc_bonus_label": "Misc",
                     "hp_hp_wounds_current_hp": "10/10",
-                    "hp_dice_label" : "HP Dice",
-                    "hp_dice" : "d10",
+                    "hp_dice_label": "HP Dice",
+                    "hp_dice": "d10",
                     "hp_contusion": "10",
                     "contusion_label": "Contusion",
                 }
         }
+        qline_editable = dict(parent=self.container, function_on_text_changed=self._update_ac)
         self.hp_total_label = create_qlabel("hp_ac_hp_total_label", self.container)
         self.hp_hp_wounds_current_hp_label = create_qlabel("hp_ac_hp_hp_wounds_current_hp_label", self.container)
         self.hp_label = create_qlabel("hp_ac_hp_label", self.container)
@@ -59,14 +60,15 @@ class HpAcBox(DefaultBox):
         self.ac_label = create_qlabel("hp_ac_ac_label", self.container)
         self.hp_dice_label = create_qlabel("hp_ac_hp_dice_label", self.container)
         self.hp_dice = create_qline_edit("hp_ac_hp_dice", self.container)
-        self.ac_total = create_qline_edit("hp_ac_ac_total", self.container)
-        self.ac_base = create_qline_edit("hp_ac_ac_base", self.container)
-        self.ac_armor_bonus = create_qline_edit("hp_ac_ac_armor_bonus", self.container)
-        self.ac_shield_bonus = create_qline_edit("hp_ac_ac_shield_bonus", self.container)
-        self.ac_dex_bonus = create_qline_edit("hp_ac_ac_dex_bonus", self.container)
-        self.ac_size_bonus = create_qline_edit("hp_ac_ac_size_bonus", self.container)
-        self.ac_misc_bonus = create_qline_edit("hp_ac_ac_misc_bonus", self.container)
+        self.ac_total = create_qline_edit("hp_ac_ac_total", self.container, enabled=False)
+        self.ac_base = create_qline_edit("hp_ac_ac_base", **qline_editable)
+        self.ac_armor_bonus = create_qline_edit("hp_ac_ac_armor_bonus", **qline_editable)
+        self.ac_shield_bonus = create_qline_edit("hp_ac_ac_shield_bonus", **qline_editable)
+        self.ac_dex_bonus = create_qline_edit("hp_ac_ac_dex_bonus", self.container, enabled=False)
+        self.ac_size_bonus = create_qline_edit("hp_ac_ac_size_bonus", **qline_editable)
+        self.ac_misc_bonus = create_qline_edit("hp_ac_ac_misc_bonus", **qline_editable)
         self.ac_total_label = create_qlabel("hp_ac_ac_total_label", self.container)
+
         self.ac_base_bonus_label = create_qlabel("hp_ac_ac_base_bonus_label", self.container)
         self.ac_armor_bonus_label = create_qlabel("hp_ac_ac_armor_bonus_label", self.container)
         self.ac_shield_bonus_label = create_qlabel("hp_ac_ac_shield_bonus_label", self.container)
@@ -96,7 +98,7 @@ class HpAcBox(DefaultBox):
 
         third_row = [self.ac_label, self.ac_total, self.ac_base,
                      self.ac_armor_bonus, self.ac_shield_bonus,
-                     self.ac_dex_bonus, self.ac_size_bonus,self.ac_misc_bonus]
+                     self.ac_dex_bonus, self.ac_size_bonus, self.ac_misc_bonus]
         add_multiple_elements_to_layout_by_row(self.layout, third_row, row=2)
 
         # TODO need to add them in another fasion (with additional offsets?)
@@ -113,3 +115,8 @@ class HpAcBox(DefaultBox):
 
     def set_values_from_attributes(self):
         self.ac_dex_bonus.setText(str(self.char_core.attributes.dex['mod']))
+        self._update_ac()
+
+    def _update_ac(self):
+        update_texts(self, "ac_total",
+                     ["ac_base", "ac_armor_bonus", "ac_shield_bonus", "ac_size_bonus", "ac_misc_bonus", "ac_dex_bonus"])
