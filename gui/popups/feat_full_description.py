9fd0a19bbb60936d58f54d774ab5cae49b4fa423
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-02-01 17:59:40 +0100

    Element instead of feat, since its now used also for spells.

--- gui/popups/feat_full_description.py
+++ gui/popups/feat_full_description.py
@@ -2,18 +2,18 @@ from PyQt5.QtWidgets import QDialog, QPlainTextEdit, QGridLayout
 
 
 class DescriptionDialog(QDialog):
-    def __init__(self, name, parent, feat):
+    def __init__(self, name, parent, element):
         super(DescriptionDialog, self).__init__(parent=parent)
         self.setWindowTitle(name)
         layout = QGridLayout()
-        self.feat = feat
+        self.element = element
         self.p = QPlainTextEdit()
-        self.p.setPlainText(self.feat._full_description)
+        self.p.setPlainText(self.element._full_description)
         self.p.setMinimumSize(200, 200)
         layout.addWidget(self.p)
         self.setBaseSize(300, 300)
         self.setLayout(layout)
 
     def closeEvent(self, evnt):
-        self.feat._full_description = self.p.document().toPlainText()
+        self.element._full_description = self.p.document().toPlainText()
         super(DescriptionDialog, self).closeEvent(evnt)
