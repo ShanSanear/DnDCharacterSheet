98fd3882b0627750e61c8764a230159492919806
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-02-09 13:08:25 +0100

    Added logging functionality.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 """
 This will be the main entry point, connecting GUI with the backend created earlier.
 """
+import logging
 
 import core
 import qt_gui
 
 
+def config_logger(logging_level):
+    logging.basicConfig(level=logging_level, style="{", format="%(asctime)s %(module)s : %f(funcName)s:%(lineno)d %(message)s", datefmt="%H:%M:%S")
+
 def main():
+    config_logger(logging.DEBUG)
     char = core.character.Character("Shan")
     attrs = dict(str=12, dex=12, con=12, int=12, wis=12, cha=16)
     char.attributes.set_attributes(attrs)
