3aac022eac45c6948d770d47866bb8d4923901a3
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-02-23 11:08:05 +0100

    Created translate reference.

--- gui/frames/tab_2/qt_spells_per_day.py
+++ gui/frames/tab_2/qt_spells_per_day.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore
 
 from gui.frames.qt_generic_classes import DefaultBox, BoxType
-from gui.frames.qt_generic_functions import create_qlabel, create_qline_edit
+from gui.frames.qt_generic_functions import create_qlabel, create_qline_edit, set_text_of_children
 
 
 class SpellsPerDayBox(BoxType, DefaultBox):
@@ -10,6 +10,11 @@ class SpellsPerDayBox(BoxType, DefaultBox):
     # TODO - adding widgets by rows/columns
     def __init__(self, parent, position, size):
         BoxType.__init__(self, parent=parent, position=position, size=size, defaults=True)
+        self.translation_reference = {"EN": {
+            "root": {"title": "Spells per day / Spells DC"},
+            "level_label": "Lvl", "per_day_label": "/Day", "extra_spells_label": "Extra", "base_spell_dc_label": "DC",
+            "_num_0": "0", "_num_1": "1", "_num_2": "2", "_num_3": "3", "_num_4": "4", "_num_5": "5", "_num_6": "6",
+            "_num_7": "7", "_num_8": "8", "_num_9": "9", }}
 
         qline_dict = dict(parent=self.container, min_size=(0, 23), align=QtCore.Qt.AlignCenter)
         qlabel_dict = dict(parent=self.container, align=QtCore.Qt.AlignCenter)
@@ -87,7 +92,7 @@ class SpellsPerDayBox(BoxType, DefaultBox):
         self._num_9 = create_qlabel(**qlabel_dict)
 
         self.add_to_layout()
-        self.translate()
+        self.translate("EN")
 
     def add_to_layout(self):
         self.layout.addWidget(self._num_0, 1, 1, 1, 1)
@@ -135,20 +140,5 @@ class SpellsPerDayBox(BoxType, DefaultBox):
         self.layout.addWidget(self.dc_lvl_9, 10, 0, 1, 1)
         self.layout.addWidget(self.extra_spells_lvl_9, 10, 3, 1, 1)
 
-    def translate(self):
-        _translate = QtCore.QCoreApplication.translate
-        self.root.setTitle(_translate("MainWindow", "Spells per day / Spells DC"))
-        self.level_label.setText(_translate("MainWindow", "Lvl"))
-        self.per_day_label.setText(_translate("MainWindow", "/Day"))
-        self.extra_spells_label.setText(_translate("MainWindow", "Extra"))
-        self.base_spell_dc_label.setText(_translate("MainWindow", "DC"))
-        self._num_0.setText(_translate("MainWindow", "0"))
-        self._num_1.setText(_translate("MainWindow", "1"))
-        self._num_2.setText(_translate("MainWindow", "2"))
-        self._num_3.setText(_translate("MainWindow", "3"))
-        self._num_4.setText(_translate("MainWindow", "4"))
-        self._num_5.setText(_translate("MainWindow", "5"))
-        self._num_6.setText(_translate("MainWindow", "6"))
-        self._num_7.setText(_translate("MainWindow", "7"))
-        self._num_8.setText(_translate("MainWindow", "8"))
-        self._num_9.setText(_translate("MainWindow", "9"))
+    def translate(self, language):
+        set_text_of_children(self, self.translation_reference[language])
