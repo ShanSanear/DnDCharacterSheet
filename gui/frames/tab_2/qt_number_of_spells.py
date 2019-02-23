301371143eef93777ade626fe4e1f3857ebfb1d9
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-02-23 10:07:53 +0100

    Generalized adding to layout. Removed TODOs.

--- gui/frames/tab_2/qt_number_of_spells.py
+++ gui/frames/tab_2/qt_number_of_spells.py
@@ -1,12 +1,11 @@
 from PyQt5 import QtCore
 
 from gui.frames.qt_generic_classes import DefaultBox, BoxType
-from gui.frames.qt_generic_functions import create_qlabel, create_qline_edit, set_text_of_children
+from gui.frames.qt_generic_functions import create_qlabel, create_qline_edit, set_text_of_children, \
+    add_multiple_elements_to_layout_by_row, add_element_to_layout
 
 
 class NumberOfSpellsBox(BoxType, DefaultBox):
-    # TODO - generalized translation
-    # TODO - adding widgets by rows/columns
     def __init__(self, parent, position, size):
         BoxType.__init__(self, parent=parent, position=position, size=size, defaults=True)
         self.translate_reference = {"EN": {"root": {"title": "Number of spells"},
@@ -20,10 +19,8 @@ class NumberOfSpellsBox(BoxType, DefaultBox):
                                            "number_of_spells_7th_label": "7th lvl",
                                            "number_of_spells_8th_label": "8th lvl",
                                            "number_of_spells_9th_label": "9th lvl",
-                                           "number_of_spells_known_number_of_spells_label": "Known number of spells (Warlock / Bard)"}}
+                                           "known_spells_title": "Known number of spells (Warlock / Bard)"}}
 
-        qline_dict = dict(parent=self.container, min_size=(0, 10))
-        qline_dict_2 = dict(parent=self.container, align=QtCore.Qt.AlignCenter)
 
         self.number_of_spells_0th_label = create_qlabel(parent=self.container)
         self.number_of_spells_1st_label = create_qlabel(parent=self.container)
@@ -35,45 +32,37 @@ class NumberOfSpellsBox(BoxType, DefaultBox):
         self.number_of_spells_7th_label = create_qlabel(parent=self.container)
         self.number_of_spells_8th_label = create_qlabel(parent=self.container)
         self.number_of_spells_9th_label = create_qlabel(parent=self.container)
-        self.number_of_spells_known_number_of_spells_label = create_qlabel(**qline_dict_2)
+        self.known_spells_title = create_qlabel(parent=self.container, align=QtCore.Qt.AlignCenter)
 
-        self.number_of_spells_0 = create_qline_edit(**qline_dict)
-        self.number_of_spells_1 = create_qline_edit(**qline_dict)
-        self.number_of_spells_2 = create_qline_edit(**qline_dict)
-        self.number_of_spells_3 = create_qline_edit(**qline_dict)
-        self.number_of_spells_4 = create_qline_edit(**qline_dict)
-        self.number_of_spells_5 = create_qline_edit(**qline_dict)
-        self.number_of_spells_6 = create_qline_edit(**qline_dict)
-        self.number_of_spells_7 = create_qline_edit(**qline_dict)
-        self.number_of_spells_8 = create_qline_edit(**qline_dict)
-        self.number_of_spells_9 = create_qline_edit(**qline_dict)
+        self.number_of_spells_0 = create_qline_edit(parent=self.container)
+        self.number_of_spells_1 = create_qline_edit(parent=self.container)
+        self.number_of_spells_2 = create_qline_edit(parent=self.container)
+        self.number_of_spells_3 = create_qline_edit(parent=self.container)
+        self.number_of_spells_4 = create_qline_edit(parent=self.container)
+        self.number_of_spells_5 = create_qline_edit(parent=self.container)
+        self.number_of_spells_6 = create_qline_edit(parent=self.container)
+        self.number_of_spells_7 = create_qline_edit(parent=self.container)
+        self.number_of_spells_8 = create_qline_edit(parent=self.container)
+        self.number_of_spells_9 = create_qline_edit(parent=self.container)
 
         self.add_to_layout()
         self.translate("EN")
         self.root.setLayout(self.layout)
 
     def add_to_layout(self):
-        self.layout.addWidget(self.number_of_spells_0th_label, 1, 0, 1, 1)
-        self.layout.addWidget(self.number_of_spells_0, 1, 1, 1, 1)
-        self.layout.addWidget(self.number_of_spells_1st_label, 1, 2, 1, 1)
-        self.layout.addWidget(self.number_of_spells_1, 1, 3, 1, 1)
-        self.layout.addWidget(self.number_of_spells_2nd_label, 1, 4, 1, 1)
-        self.layout.addWidget(self.number_of_spells_2, 1, 5, 1, 1)
-        self.layout.addWidget(self.number_of_spells_3rd_label, 1, 6, 1, 1)
-        self.layout.addWidget(self.number_of_spells_3, 1, 7, 1, 1)
-        self.layout.addWidget(self.number_of_spells_4th_label, 1, 8, 1, 1)
-        self.layout.addWidget(self.number_of_spells_4, 1, 9, 1, 1)
-        self.layout.addWidget(self.number_of_spells_5th_label, 2, 0, 1, 1)
-        self.layout.addWidget(self.number_of_spells_5, 2, 1, 1, 1)
-        self.layout.addWidget(self.number_of_spells_6th_label, 2, 2, 1, 1)
-        self.layout.addWidget(self.number_of_spells_6, 2, 3, 1, 1)
-        self.layout.addWidget(self.number_of_spells_7th_label, 2, 4, 1, 1)
-        self.layout.addWidget(self.number_of_spells_7, 2, 5, 1, 1)
-        self.layout.addWidget(self.number_of_spells_8th_label, 2, 6, 1, 1)
-        self.layout.addWidget(self.number_of_spells_8, 2, 7, 1, 1)
-        self.layout.addWidget(self.number_of_spells_9th_label, 2, 8, 1, 1)
-        self.layout.addWidget(self.number_of_spells_9, 2, 9, 1, 1)
-        self.layout.addWidget(self.number_of_spells_known_number_of_spells_label, 0, 2, 1, 6)
+        add_element_to_layout(self.layout, self.known_spells_title, 0, 2, 1, 6)
+
+        first_row = [self.number_of_spells_0th_label, self.number_of_spells_0, self.number_of_spells_1st_label,
+                     self.number_of_spells_1, self.number_of_spells_2nd_label, self.number_of_spells_2,
+                     self.number_of_spells_3rd_label, self.number_of_spells_3, self.number_of_spells_4th_label,
+                     self.number_of_spells_4]
+        add_multiple_elements_to_layout_by_row(self.layout, first_row, row=1)
+
+        second_row = [self.number_of_spells_5th_label, self.number_of_spells_5, self.number_of_spells_6th_label,
+                      self.number_of_spells_6, self.number_of_spells_7th_label, self.number_of_spells_7,
+                      self.number_of_spells_8th_label, self.number_of_spells_8, self.number_of_spells_9th_label,
+                      self.number_of_spells_9]
+        add_multiple_elements_to_layout_by_row(self.layout, second_row, row=2)
 
     def translate(self, language):
         set_text_of_children(self, self.translate_reference[language])
