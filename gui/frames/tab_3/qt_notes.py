0a9c5c5ea8d30d438ec939bcea82b3a1f62d1949
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-03-03 13:19:06 +0100

    Changed margins for notes.

--- gui/frames/tab_3/qt_notes.py
+++ gui/frames/tab_3/qt_notes.py
@@ -6,7 +6,7 @@ from gui.frames.qt_generic_functions import set_text_of_children, create_qlabel
 
 class NotesBox(BoxType, DefaultBox):
     def __init__(self, parent, position, size):
-        BoxType.__init__(self, parent=parent, position=position, size=size)
+        BoxType.__init__(self, parent=parent, position=position, size=size, margins=[10, 10, 10, 10])
         self.translate_reference = {
             "EN":
                 {
@@ -14,21 +14,6 @@ class NotesBox(BoxType, DefaultBox):
                         "title": "Notes",
                     },
                     "notes_label": "Notes",
-                    "notes": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien urna, "
-                             "egestas eu tempor at, pretium nec orci. In nec pharetra tellus. In malesuada erat "
-                             "tellus, eget efficitur elit convallis eu. Integer consectetur porttitor eros vitae "
-                             "sagittis. Vestibulum commodo suscipit varius. Nulla vitae fringilla velit. Mauris "
-                             "sagittis tellus urna. Class aptent taciti sociosqu ad litora torquent per conubia "
-                             "nostra, per inceptos himenaeos. Fusce in arcu justo.\n\nMauris auctor, "
-                             "elit sed tristique maximus, libero quam feugiat dolor, in laoreet massa libero a "
-                             "tortor. Donec molestie pretium tempus. Integer lacinia magna lacus, nec volutpat felis "
-                             "condimentum id. Vivamus orci ante, gravida sed velit vel, venenatis pellentesque est. "
-                             "Maecenas laoreet laoreet ullamcorper. Phasellus rutrum felis non sapien aliquam "
-                             "semper. Morbi tempus, quam in luctus tempor, mauris neque faucibus libero, "
-                             "ut porta justo sem eu orci. Suspendisse consectetur sem ac neque mollis, sed iaculis "
-                             "tellus elementum. Nulla facilisis turpis vel sagittis pharetra. Donec nec odio "
-                             "pharetra, vestibulum lorem accumsan, tempus nulla. Donec blandit nisi in elit "
-                             "pulvinar, nec feugiat metus egestas. "
                 }
         }
         self.notes_label = create_qlabel(self.container, align=QtCore.Qt.AlignCenter)
