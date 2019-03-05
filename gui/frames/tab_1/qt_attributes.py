474fd5fc399bb16a23d12514d7dc54d220e1802a
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-03-05 19:41:39 +0100

    Removed done todo.

--- gui/frames/tab_1/qt_attributes.py
+++ gui/frames/tab_1/qt_attributes.py
@@ -12,8 +12,6 @@ class AttributesBox(BoxType, DefaultBox):
     char_core: Character
 
     def __init__(self, parent, position, size, char_core):
-        # TODO Temporary attributes mods
-
         self.char_core = char_core
         BoxType.__init__(self, parent=parent, position=position, size=size)
         self.attrs_names = ('str', 'dex', 'con', 'int', 'wis', 'cha')
