2d330aa7f9040dce7900327ed381c3aa118ad670
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-03-26 16:06:09 +0100

    Cleaned up feats.

--- gui/frames/tab_1/qt_feats.py
+++ gui/frames/tab_1/qt_feats.py
@@ -22,18 +22,15 @@ class FeatsBox(ScrollableBox):
         self.add_new = create_push_button("add_new_feat", self.container, min_size=[20, 20], max_size=[20, 20],
                                           text="+")
         self.last_row = [self.add_new]
-        self.translate_reference = {"EN":
-            {"root":
+        self.translate_reference = {
+            "EN":
+                {
+                    "root":
                 {
                     "title": "Feats / Special abilities"
                 },
                 "description_label": "Desc",
                 "name_label": "Feat name",
-            }
-        }
-        self.translate_reference_new_element = {
-            "EN": {
-                "description_button": "..."
             }
         }
 
@@ -69,14 +66,13 @@ class FeatsBox(ScrollableBox):
         new_feat.name = create_qline_edit(self.container, max_size=(150, None),
                                           function_on_unfocused=self.sort_elements)
 
-        # new_feat.description_edit = create_qline_edit(self.container)
         new_feat.description_button = create_push_button(f"feat_{idx}_description_button",
-                                                         self.container, min_size=[20, 20], max_size=[20, 20], )
+                                                         self.container, min_size=[20, 20], max_size=[20, 20],
+                                                         text="...")
         new_feat.description_button.clicked.connect(partial(self.show_description, new_feat))
-        new_feat._full_description = "ABCD"
+        new_feat._full_description = ""
         new_feat.delete_feat = create_push_button("item_delete", self.container, min_size=[20, 20], max_size=[20, 20],
                                                   text="-",
                                                   function_on_clicked=self._remove_element, args_on_clicked=new_feat)
-        set_text_of_children(new_feat, self.translate_reference_new_element["EN"])
 
         return new_feat
