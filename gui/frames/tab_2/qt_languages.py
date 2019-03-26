7258f2de61b8ddbb9c5512243d11fbcb5973834f
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-03-26 18:10:30 +0100

    Removed filling text from known_languages.

--- gui/frames/tab_2/qt_languages.py
+++ gui/frames/tab_2/qt_languages.py
@@ -18,21 +18,6 @@ class LanguagesBox(BoxType, DefaultBox):
                     "title": "Languages",
                 },
                 "known_languages_label": "Known Languages",
-                "known_languages": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien urna, "
-                                   "egestas eu tempor at, pretium nec orci. In nec pharetra tellus. In malesuada erat "
-                                   "tellus, eget efficitur elit convallis eu. Integer consectetur porttitor eros "
-                                   "vitae sagittis. Vestibulum commodo suscipit varius. Nulla vitae fringilla velit. "
-                                   "Mauris sagittis tellus urna. Class aptent taciti sociosqu ad litora torquent per "
-                                   "conubia nostra, per inceptos himenaeos. Fusce in arcu justo.\n\nMauris auctor, "
-                                   "elit sed tristique maximus, libero quam feugiat dolor, in laoreet massa libero a "
-                                   "tortor. Donec molestie pretium tempus. Integer lacinia magna lacus, nec volutpat "
-                                   "felis condimentum id. Vivamus orci ante, gravida sed velit vel, venenatis "
-                                   "pellentesque est. Maecenas laoreet laoreet ullamcorper. Phasellus rutrum felis "
-                                   "non sapien aliquam semper. Morbi tempus, quam in luctus tempor, mauris neque "
-                                   "faucibus libero, ut porta justo sem eu orci. Suspendisse consectetur sem ac neque "
-                                   "mollis, sed iaculis tellus elementum. Nulla facilisis turpis vel sagittis "
-                                   "pharetra. Donec nec odio pharetra, vestibulum lorem accumsan, tempus nulla. Donec "
-                                   "blandit nisi in elit pulvinar, nec feugiat metus egestas. "
             }
         }
         self.translate("EN")
