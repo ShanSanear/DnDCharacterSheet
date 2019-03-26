c97f852b7a252899453d785941544098646e6cf6
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-03-26 16:55:28 +0100

    Added basic menu items for changing languages.

--- gui/frames/qt_menu_bar.py
+++ gui/frames/qt_menu_bar.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QActionGroup
 
 from gui.frames.qt_generic_functions import set_text_of_children
 
@@ -8,6 +9,7 @@ class MenuBar:
         self.root = QtWidgets.QMenuBar(main_window)
         self.file_menu = QtWidgets.QMenu(self.root)
         self.about_menu = QtWidgets.QMenu(self.root)
+        self.language_menu = QtWidgets.QMenu(self.root)
         main_window.setMenuBar(self.root)
         # TODO NEW CHARACTER - 10S ATTRIBUTES
 
@@ -19,22 +21,34 @@ class MenuBar:
         self.open_character.setShortcut("Ctrl+o")
         self.save_character.setShortcut("Ctrl+s")
         self.save_character_as.setShortcut("Ctrl+Alt+s")
+        self.language_action_group = QActionGroup(main_window, exclusive=True)
+        self.change_language_en = QtWidgets.QAction(main_window, checkable=True)
+        self.change_language_pl = QtWidgets.QAction(main_window, checkable=True)
+        self.language_action_group.addAction(self.change_language_en)
+        self.language_action_group.addAction(self.change_language_pl)
+        self.change_language_en.setChecked(True)
         self.exit = QtWidgets.QAction(main_window)
         self.file_menu.addAction(self.new_character)
         self.file_menu.addAction(self.open_character)
         self.file_menu.addAction(self.save_character)
         self.file_menu.addAction(self.save_character_as)
         self.file_menu.addAction(self.exit)
+        self.language_menu.addAction(self.change_language_en)
+        self.language_menu.addAction(self.change_language_pl)
         self.root.addAction(self.file_menu.menuAction())
+        self.root.addAction(self.language_menu.menuAction())
         self.root.addAction(self.about_menu.menuAction())
         self.translate_reference = {
             "EN": {
                 "file_menu": {"title": "File"},
                 "about_menu": {"title": "About"},
+                "language_menu": {"title": "Language"},
                 "new_character": "New character...",
                 "open_character": "Open",
                 "save_character": "Save",
                 "save_character_as": "Save as...",
+                "change_language_en": "English",
+                "change_language_pl": "polski",
                 "exit": "Exit",
             }
         }
