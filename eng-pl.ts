97479caf141fdab130fad1743113c66e79bf5b68
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-04-03 20:08:14 +0200

    Adding missing qm and ts files.

---
+++ eng-pl.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL" sourcelanguage="en_GB">
+<context>
+    <name>BasicInfo</name>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="112"/>
+        <source>Class</source>
+        <translation>Klasa</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="113"/>
+        <source>Faith</source>
+        <translation>Wiara</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="114"/>
+        <source>Race</source>
+        <translation>Rasa</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="115"/>
+        <source>Alignement</source>
+        <translation>Charakter</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="116"/>
+        <source>Player</source>
+        <translation>Gracz</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="117"/>
+        <source>Name</source>
+        <translation>Imię</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="118"/>
+        <source>Height</source>
+        <translation>Wzrost</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="119"/>
+        <source>Weight</source>
+        <translation>Waga</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="120"/>
+        <source>Experience</source>
+        <translation>Doświadczenie</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="121"/>
+        <source>Eyes</source>
+        <translation>Oczy</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="122"/>
+        <source>Hair</source>
+        <translation>Włosy</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="123"/>
+        <source>Size</source>
+        <translation>Rozm.</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="124"/>
+        <source>Age</source>
+        <translation>Wiek</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="125"/>
+        <source>Gender</source>
+        <translation>Płeć</translation>
+    </message>
+    <message>
+        <location filename="gui/frames/tab_1/qt_basic_info.py" line="126"/>
+        <source>Level</source>
+        <translation>Poziom</translation>
+    </message>
+</context>
+<context>
+    <name>Tabs</name>
+    <message>
+        <location filename="gui/main_window.py" line="64"/>
+        <source>Basic information / Combat / Skills / Feats</source>
+        <translation type="obsolete">Bazowe info / Walka / Umiejętności / Atuty</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="77"/>
+        <source>Items / Spells / Languages</source>
+        <translation type="obsolete">Przedmioty / Czary / Języki</translation>
+    </message>
+    <message>
+        <location filename="gui/main_window.py" line="91"/>
+        <source>Weapons / Armor / Notes</source>
+        <translation type="obsolete">Bronie / Pancerz / Notatki</translation>
+    </message>
+</context>
+</TS>
