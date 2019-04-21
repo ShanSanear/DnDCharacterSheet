d03e50abd211db32f4fc91ec30ffd2f8698da5fd
shan <medan1993@gmail.com>
2019-04-21 18:27:08 +0200

    Added constants to be used in settings.

---
+++ gui/constants.py
@@ -0,0 +1,5 @@
+APP_NAME_SINGLE = "Single DnD Character Sheet"
+APP_NAME_MULTI = "Multi DnD Character Sheet"
+ORG_NAME = "DnDCharacterSheet"
+MAIN_WINDOW_SIZE = "main_window_size"
+MAIN_WINDOW_POSITION = "main_window_position"
