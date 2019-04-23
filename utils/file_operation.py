29d8e354ebc7d847c30f143ff0c293d62a078d5e
Mateusz Pruski <pruskimateusz93@gmail.com>
2019-04-23 18:05:49 +0200

    Now only 1 backup is created.

--- utils/file_operation.py
+++ utils/file_operation.py
@@ -5,11 +5,13 @@ from pathlib import Path
 
 
 def file_backup_same_dir(src_file_path: Path, max_backups: int = 5):
+    src_file_path = Path(src_file_path)
     backup_paths = [Path(src_file_path.with_suffix(f'.bak{idx}')) for idx in range(max_backups)]
     for file in backup_paths:
         if not file.exists():
             logging.debug("Copying file from %s to %s", src_file_path, file)
             shutil.copy(src_file_path, file)
+            return
     backup_paths.sort(key=lambda x: os.lstat(x).st_ctime)
     dest_path = backup_paths[0]
     logging.debug("Removing backup file: %s", dest_path)
